0f698a1d51e5d79dac59a8bfeb0b7893dc5606cc
xusai <xusai@chinatelecom.cn>
2025-05-12 10:13:01 +0800

    response: fix bind response

--- pkg/openapi/resonse.go
+++ pkg/openapi/resonse.go
@@ -1,7 +1,15 @@
 package openapi
 
-type CtapiResponse struct {
-	StatusCode string `json:"statusCode,omitempty"`
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/tidwall/gjson"
+)
+
+type Response struct {
+	StatusCode interface{} `json:"statusCode,omitempty"`
 	// Error code, which is a three part code for product.module.code
 	Error string `json:"error,omitempty"`
 	// Error description during failure, usually in English
@@ -12,14 +20,38 @@ type CtapiResponse struct {
 	ReturnObj interface{} `json:"returnObj"`
 }
 
-type OpenapiResponse struct {
-	StatusCode int `json:"statusCode,omitempty"`
-	// Error code, which is a three part code for product.module.code
-	Error string `json:"error,omitempty"`
-	// Error description during failure, usually in English
-	Message string `json:"message,omitempty"`
-	// Error description during failure, usually in Chinese
-	Description string `json:"description,omitempty"`
-	// Data returned upon success
-	ReturnObj interface{} `json:"returnObj"`
+func (r *Response) DeepCopy() Response {
+	return Response{
+		StatusCode:  r.StatusCode,
+		Error:       r.Error,
+		Message:     r.Message,
+		Description: r.Description,
+		ReturnObj:   r.ReturnObj,
+	}
+}
+
+func (r *Response) ParseStatusCode() int {
+	switch v := r.StatusCode.(type) {
+	case string:
+		i, _ := strconv.Atoi(v)
+		return i
+	case int:
+		return v
+	default:
+		return 900
+	}
+}
+
+func (r *Response) BindReturnObj(jsonStr string) error {
+	value := gjson.Get(jsonStr, "returnObj")
+	if value.IsObject() {
+		return json.Unmarshal([]byte(value.String()), &r.ReturnObj)
+	}
+
+	if value.IsArray() || value.IsBool() || value.Type == gjson.Number || value.Type == gjson.String {
+		buildData := fmt.Sprintf("{\"data\": %s}", value.Raw)
+		return json.Unmarshal([]byte(buildData), &r.ReturnObj)
+	}
+
+	return nil
 }
---
+++ pkg/openapi/response_test.go
@@ -0,0 +1,113 @@
+package openapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type SliceReturnObj struct {
+	Data []map[string]interface{} `json:"data,omitempty"`
+}
+
+type BoolReturnObj struct {
+	Data bool `json:"data,omitempty"`
+}
+
+type NumberReturnObj struct {
+	Data int64 `json:"data,omitempty"`
+}
+
+type StringReturnObj struct {
+	Data string `json:"data,omitempty"`
+}
+
+type ReturnObj struct {
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
+}
+
+func TestUnmarshalReturnObj(t *testing.T) {
+	// slice
+	sliceObj := &SliceReturnObj{}
+	response := Response{}
+	response.ReturnObj = sliceObj
+	jsonData := `{"statusCode": 800,"returnObj": [{ "id": 1, "name": "A" },{ "id": 2, "name": "B" }]}`
+	err := bind(jsonData, response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// bool
+	boolObj := &BoolReturnObj{}
+	response = response.DeepCopy()
+	response.ReturnObj = boolObj
+	boolData := `{"statusCode": 800,"returnObj": true}`
+	err = bind(boolData, response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// number
+	numberObj := &NumberReturnObj{}
+	response = response.DeepCopy()
+	response.ReturnObj = numberObj
+	numberData := `{"statusCode": 800,"returnObj": 123}`
+	err = bind(numberData, response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// string
+	stringObj := &StringReturnObj{}
+	response = response.DeepCopy()
+	response.ReturnObj = stringObj
+	strData := `{"statusCode": 800,"returnObj": "test"}`
+	err = bind(strData, response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// object
+	obj := &ReturnObj{}
+	response = response.DeepCopy()
+	response.ReturnObj = obj
+	objectData := `{"statusCode": 800,"returnObj": {"name": "test", "age": 18}}`
+	err = bind(objectData, response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func bind(data string, response Response) error {
+	err := response.BindReturnObj(data)
+	if err != nil {
+		return err
+	}
+	fmt.Println(response.ReturnObj)
+	return nil
+}
+
+func TestUnmarshalStatusCode(t *testing.T) {
+	response := Response{}
+	strCode := `{"statusCode": "800"}`
+	err := json.Unmarshal([]byte(strCode), &response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(response.StatusCode)
+
+	intCode := `{"statusCode": 800}`
+	err = json.Unmarshal([]byte(intCode), &response)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(response.StatusCode)
+}
